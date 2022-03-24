2b316989d75645f503a8aaed9696b3835e1b643b
Knight <liuxiang147@163.com>
2022-03-25 00:38:14 +0800

    LeetCode-Go

--- leetcode/editor/cn/[412]Fizz Buzz.go
+++ leetcode/editor/cn/[412]Fizz Buzz.go
@@ -45,7 +45,19 @@ func fizzBuzz(n int) []string {
 	//---------------------------------模拟----------------------------------
 	// Time: O()
 	// Space: O()
-	for
+	ans := []string{}
+	for i := 1 ; i <= n ; i++ {
+		if i % 3 == 0 && i % 5 == 0 {
+			ans = append(ans , "FizzBuzz")
+		}else if i % 3 == 0 {
+			ans = append(ans , "Fizz")
+		}else if i % 5 == 0 {
+			ans = append(ans , "Buzz")
+		}else{
+			ans = append(ans , strconv.Itoa(i))
+		}
+	}
+	return ans
 	//----------------------------------------------------------------------
 }
 //leetcode submit region end(Prohibit modification and deletion)
